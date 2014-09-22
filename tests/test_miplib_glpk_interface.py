48427c7cb31fe106e0a5885e70788417e675169d
Joao Cardoso <joaca@biosustain.dtu.dk>
2014-09-22 11:49:53 +0200

    using problems from miplib2003

--- tests/test_miplib_glpk_interface.py
+++ tests/test_miplib_glpk_interface.py
@@ -21,14 +21,14 @@ import tempfile
 import urllib2
 import nose
 from optlang.glpk_interface import Model
-from swiglpk import glp_read_mps, GLP_MPS_FILE, glp_create_prob, glp_get_num_cols, GLP_BR_PCH
+from swiglpk import glp_read_mps, GLP_MPS_FILE, glp_create_prob, glp_get_num_cols, GLP_BR_PCH, glp_read_sol
 
 MULTISPACE_RE = re.compile("\s+")
-MIPLIB_URL = "http://miplib.zib.de/download/%s"
+MIPLIB_URL = "http://miplib.zib.de/miplib2003/download/%s"
 
 DATA_PATH = os.path.join(os.path.dirname(__file__), 'data')
-SOLUTION = os.path.join(DATA_PATH, "miplib_benchmark.solutions")
-PROBLEMS_DIR = os.path.join(DATA_PATH, "miplib2010")
+SOLUTION = os.path.join(DATA_PATH, "miplib2003.txt")
+PROBLEMS_DIR = os.path.join(DATA_PATH, "miplib2003")
 
 SOLUTION_MAPPING = {
     "=opt=": "optimal",
@@ -43,19 +43,27 @@ def download_file(file_name, problem_dir):
     _in.close()
 
 
-def load_problem(mps_file):
-    tmp_file = tempfile.mktemp(suffix='.mps')
-    with open(tmp_file, 'w') as tmp_handle:
+def load_problem(mps_file, warmup_file):
+    prob_tmp_file = tempfile.mktemp(suffix='.mps')
+    with open(prob_tmp_file, 'w') as tmp_handle:
         f = gzip.open(mps_file, 'rb')
         tmp_handle.write(f.read())
         f.close()
 
+    sol_temp_file = tempfile.mktemp(suffix='.bas')
+    with open(sol_temp_file, 'w') as tmp_handle:
+        f = gzip.open(warmup_file, 'rb')
+        tmp_handle.write(f.read())
+        f.close()
+
     problem = glp_create_prob()
-    glp_read_mps(problem, GLP_MPS_FILE, None, tmp_file)
+    glp_read_mps(problem, GLP_MPS_FILE, None, prob_tmp_file)
+    #warmup
+    glp_read_sol(problem, sol_temp_file)
     model = Model(problem=problem)
     model.configuration.presolve = True
     model.configuration.verbosity = 3
-    model.configuration.timeout = 60 * 10
+    model.configuration.timeout = 60 * 3
     model.configuration._iocp.br_tech = GLP_BR_PCH
     return problem, model
 
@@ -109,33 +117,36 @@ def test_miplib(solutions=SOLUTION, problem_dir=PROBLEMS_DIR):
                 sol_type, name, f = data[0], data[1], data[2]
             else:
                 sol_type, name, f = data[0], data[1], None
-            if name == "air04":
-                sol_type = SOLUTION_MAPPING[sol_type]
-                print sol_type
-                expected_solution = MockupSolution(name, sol_type, f, problem_dir)
-                problem_file = os.path.join(problem_dir, "%s.mps.gz" % name)
-                try:
-                    if not os.path.exists(problem_file):
-                        download_file("%s.mps.gz" % name, problem_dir)
-                except:
-                    continue
+            sol_type = SOLUTION_MAPPING[sol_type]
+            print "%s: %s" % (name, sol_type)
+            expected_solution = MockupSolution(name, sol_type, f, problem_dir)
+            problem_file = os.path.join(problem_dir, "%s.mps.gz" % name)
+            warmup_file = os.path.join(problem_dir, "%s.bas.gz" % name)
+            try:
+                if not os.path.exists(problem_file):
+                    download_file("%s.mps.gz" % name, problem_dir)
+                if not os.path.exists(warmup_file):
+                    download_file("%s.bas.gz" % name, problem_dir)
+            except Exception as e:
+                raise e
 
-                glpk_problem, model = load_problem(problem_file)
-                status = model.optimize()
-                func = partial(check_dimensions, model, glpk_problem)
-                func.description = "test_miplib_check_dimensions_%s (%s)" % (name, os.path.basename(str(__file__)))
+            glpk_problem, model = load_problem(problem_file, warmup_file)
+            status = model.optimize()
+            func = partial(check_dimensions, model, glpk_problem)
+            func.description = "test_miplib_check_dimensions_%s (%s)" % (name, os.path.basename(str(__file__)))
+            yield func
+            print "%s: %s" % (name, status)
+            if status == "timelimit":
+                continue
+
+            func = partial(check_optimization, status, model, expected_solution)
+            func.description = "test_miplib_check_optimization_%s (%s)" % (name, os.path.basename(str(__file__)))
+            yield func
+
+            if expected_solution.sol_type == "optimal":
+                func = partial(check_variables, model, expected_solution)
+                func.description = "test_miplib_check_variables_%s (%s)" % (name, os.path.basename(str(__file__)))
                 yield func
-                if status == "timeout":
-                    continue
-
-                func = partial(check_optimization, status, model, expected_solution)
-                func.description = "test_miplib_check_optimization_%s (%s)" % (name, os.path.basename(str(__file__)))
-                yield func
-
-                if expected_solution.sol_type == "optimal":
-                    func = partial(check_variables, model, expected_solution)
-                    func.description = "test_miplib_check_variables_%s (%s)" % (name, os.path.basename(str(__file__)))
-                    yield func
 
 
 if __name__ == '__main__':
