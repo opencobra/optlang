e28d9e1803e67bddd573fe157df449495694e64e
Joao Cardoso <joaca@biosustain.dtu.dk>
2014-09-22 15:35:50 +0200

    needs to implement variable comparison for problems that converge

--- tests/test_miplib_glpk_interface.py
+++ tests/test_miplib_glpk_interface.py
@@ -23,6 +23,8 @@ import nose
 from optlang.glpk_interface import Model
 from swiglpk import glp_read_mps, GLP_MPS_FILE, glp_create_prob, glp_get_num_cols, GLP_BR_PCH, glp_read_sol
 
+#problems from http://miplib.zib.de/miplib2003/miplib2003.php
+
 MULTISPACE_RE = re.compile("\s+")
 MIPLIB_URL = "http://miplib.zib.de/miplib2003/download/%s"
 
@@ -43,27 +45,19 @@ def download_file(file_name, problem_dir):
     _in.close()
 
 
-def load_problem(mps_file, warmup_file):
+def load_problem(mps_file,):
     prob_tmp_file = tempfile.mktemp(suffix='.mps')
     with open(prob_tmp_file, 'w') as tmp_handle:
         f = gzip.open(mps_file, 'rb')
         tmp_handle.write(f.read())
         f.close()
 
-    sol_temp_file = tempfile.mktemp(suffix='.bas')
-    with open(sol_temp_file, 'w') as tmp_handle:
-        f = gzip.open(warmup_file, 'rb')
-        tmp_handle.write(f.read())
-        f.close()
-
     problem = glp_create_prob()
     glp_read_mps(problem, GLP_MPS_FILE, None, prob_tmp_file)
-    #warmup
-    glp_read_sol(problem, sol_temp_file)
     model = Model(problem=problem)
     model.configuration.presolve = True
     model.configuration.verbosity = 3
-    model.configuration.timeout = 60 * 3
+    model.configuration.timeout = 60 * 7
     model.configuration._iocp.br_tech = GLP_BR_PCH
     return problem, model
 
@@ -72,15 +66,16 @@ def check_dimensions(model, glpk_problem):
     nose.tools.assert_true(glp_get_num_cols(glpk_problem) == len(model.variables))
 
 
-def check_optimization(status, model, expected_solution):
-    nose.tools.assert_equals(status, expected_solution.sol_type)
-    if status is "optimal":
-        nose.tools.assert_almost_equal(expected_solution.f, model.objective.value, places=4)
+def check_optimization(model, expected_solution):
+    status = model.optimize()
+    if not status is "time_limit":
+        nose.tools.assert_equals(status, expected_solution.sol_type)
 
+        if status is "optimal":
+            nose.tools.assert_almost_equal(expected_solution.f, model.objective.value, places=4)
 
-def check_variables(model, expected_solution):
-    for v in model.variables:
-        nose.tools.assert_almost_equal(expected_solution.solutions[v.id], v.primal, places=4)
+            # for v in model.variables:
+            #     nose.tools.assert_almost_equal(expected_solution.solution[v.name], v.primal, places=4)
 
 
 class MockupSolution():
@@ -88,7 +83,7 @@ class MockupSolution():
         self.name = name
         self.sol_type = sol_type
         self.f = f
-        self.problems_dir = problem_dir
+        self.problem_dir = problem_dir
         self._solution = None
 
     @property
@@ -98,13 +93,13 @@ class MockupSolution():
             solution_file = os.path.join(self.problem_dir, "%s.sol.gz" % self.name)
             try:
                 if not os.path.exists(solution_file):
-                    download_file("%s.sol.gz" % self.name, self.problems_dir)
+                    download_file("%s.sol.gz" % self.name, self.problem_dir)
                 f = gzip.open(solution_file, 'rb')
                 for line in f:
                     data = MULTISPACE_RE.split(line)
-                    self._solution[data[0]] = float(data[1])
-            except:
-                pass
+                    self._solution[data[1]] = float(data[2])
+            except Exception as e:
+                raise e
 
         return self._solution
 
@@ -113,40 +108,25 @@ def test_miplib(solutions=SOLUTION, problem_dir=PROBLEMS_DIR):
     with open(solutions, "rU") as fhandler:
         for line in fhandler:
             data = MULTISPACE_RE.split(line)
-            if len(data) == 3:
-                sol_type, name, f = data[0], data[1], data[2]
-            else:
-                sol_type, name, f = data[0], data[1], None
+            sol_type, name, f = data[0], data[1], float(data[2])
             sol_type = SOLUTION_MAPPING[sol_type]
-            print "%s: %s" % (name, sol_type)
             expected_solution = MockupSolution(name, sol_type, f, problem_dir)
             problem_file = os.path.join(problem_dir, "%s.mps.gz" % name)
-            warmup_file = os.path.join(problem_dir, "%s.bas.gz" % name)
             try:
                 if not os.path.exists(problem_file):
                     download_file("%s.mps.gz" % name, problem_dir)
-                if not os.path.exists(warmup_file):
-                    download_file("%s.bas.gz" % name, problem_dir)
+
             except Exception as e:
                 raise e
 
-            glpk_problem, model = load_problem(problem_file, warmup_file)
-            status = model.optimize()
+            glpk_problem, model = load_problem(problem_file)
             func = partial(check_dimensions, model, glpk_problem)
-            func.description = "test_miplib_check_dimensions_%s (%s)" % (name, os.path.basename(str(__file__)))
-            yield func
-            print "%s: %s" % (name, status)
-            if status == "timelimit":
-                continue
-
-            func = partial(check_optimization, status, model, expected_solution)
-            func.description = "test_miplib_check_optimization_%s (%s)" % (name, os.path.basename(str(__file__)))
+            func.description = "test_miplib_dimensions_%s (%s)" % (name, os.path.basename(str(__file__)))
             yield func
 
-            if expected_solution.sol_type == "optimal":
-                func = partial(check_variables, model, expected_solution)
-                func.description = "test_miplib_check_variables_%s (%s)" % (name, os.path.basename(str(__file__)))
-                yield func
+            func = partial(check_optimization, model, expected_solution)
+            func.description = "test_miplib_optimization_%s (%s)" % (name, os.path.basename(str(__file__)))
+            yield func
 
 
 if __name__ == '__main__':
