02a3300d99cf880212fefa15913e2e3f7b720d65
KristianJensen <KristianJensen@users.noreply.github.com>
2017-01-31 11:19:27 +0100

    fix: bug when changing variable names (#63)
    Only update the _variables_to_constriants_mapping dict when new_name !=
    old_name

--- optlang/interface.py
+++ optlang/interface.py
@@ -209,7 +209,7 @@ class Variable(sympy.Symbol):
     def name(self, value):
         old_name = getattr(self, 'name', None)
         self._name = value
-        if getattr(self, 'problem', None) is not None:
+        if getattr(self, 'problem', None) is not None and value != old_name:
             self.problem.variables.update_key(old_name)
             self.problem._variables_to_constraints_mapping[value] = self.problem._variables_to_constraints_mapping[old_name]
             del self.problem._variables_to_constraints_mapping[old_name]
