bf1fc05397e1f7de2279078a1ffd0339cbfd37fe
long-m-r <long-m-r@users.noreply.github.com>
2017-03-30 11:03:47 -0500

    Suggested Changes

--- optlang/__init__.py
+++ optlang/__init__.py
@@ -16,6 +16,7 @@
 from __future__ import absolute_import
 
 import logging
+import traceback
 from optlang._version import get_versions
 from optlang.util import list_available_solvers
 from optlang.interface import statuses
@@ -30,44 +31,41 @@ log = logging.getLogger(__name__)
 available_solvers = list_available_solvers()
 
 # Try to load each solver.
-try:
-	from optlang import glpk_interface
-except:
-	glpk_interface = None
-	if available_solvers['GLPK']:
-		log.error('GLPK could not load but should be available')
+if available_solvers['GLPK']:
+    try:
+        from optlang import glpk_interface
+    except Exception:
+        log.error('GLPK is available but could not load with error:\n  ' + str(traceback.format_exc()).strip().replace('\n','\n  '))
 
-try:
-	from optlang import cplex_interface
-except:
-	cplex_interface = None
-	if available_solvers['CPLEX']:
-		log.error('CPLEX could not load but should be available')
+if available_solvers['CPLEX']:
+    try:
+        from optlang import cplex_interface
+    except Exception:
+        log.error('CPLEX is available but could not load with error:\n  ' + str(traceback.format_exc()).strip().replace('\n','\n  '))
 
-try:
-	from optlang import gurobi_interface
-except:
-	gurobi_interface = None
-	if available_solvers['GUROBI']:
-		log.error('GUROBI could not load but should be available')
-try:
-	from optlang import scipy_interface
-except:
-	scipy_interface = None
-	if available_solvers['SCIPY']:
-		log.error('SCIPY could not load but should be available')
+if available_solvers['GUROBI']:
+    try:
+        from optlang import gurobi_interface
+    except Exception:
+        log.error('GUROBI is available but could not load with error:\n  ' + str(traceback.format_exc()).strip().replace('\n','\n  '))
+
+if available_solvers['SCIPY']:
+    try:
+        from optlang import scipy_interface
+    except Exception:
+        log.error('SCIPY is available but could not load with error:\n  ' + str(traceback.format_exc()).strip().replace('\n','\n  '))
+
 
 # Go through and find the best solver that loaded. Load that one as the default
-best_interface = None
-for engine in [cplex_interface,gurobi_interface,glpk_interface,scipy_interface]:
-	if engine is not None:
-		best_interface = engine
-		Model = engine.Model
-		Variable = engine.Variable
-		Constraint = engine.Constraint
-		Objective = engine.Objective
-		break
-
-# If we can't find any interface, that is probably an issue
-if best_interface is None:
-	log.error('No solvers were available and/or loadable.')
+for engine_str in ['cplex_interface','gurobi_interface','glpk_interface','scipy_interface']:
+    # Must check globals since not all interface variables will be defined
+    if engine_str in globals():
+        engine=globals()[engine_str]
+        Model = engine.Model
+        Variable = engine.Variable
+        Constraint = engine.Constraint
+        Objective = engine.Objective
+        break
+else:
+    # We were unable to find any valid solvers
+    log.error('No solvers were available and/or loadable.')
