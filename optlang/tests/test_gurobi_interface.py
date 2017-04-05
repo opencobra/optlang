98bfa010974905a24c8df30759f6199b6f52f34e
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2017-04-05 10:17:20 +0200

    feat: add dual values tests in MIP to gurobi

--- optlang/tests/test_gurobi_interface.py
+++ optlang/tests/test_gurobi_interface.py
@@ -422,5 +422,12 @@ else:
                 if col_name == 'R_Biomass_Ecoli_core_w_GAM':
                     self.assertEqual(row.getCoeff(i), 666.)
 
+
+    class DualValuesMIPTestCase(
+        abstract_test_cases.AbstractDualValuesMIPTestCase):
+
+        interface = gurobi_interface
+
+
 if __name__ == '__main__':
     nose.runmodule()
