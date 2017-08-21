b59c7f138880c61de7205b0f859ae470e428bc6c
Kristian Jensen <jensenkristian89@gmail.com>
2017-08-21 11:24:47 +0200

    test: adapt expression_parsing tests to symengine

--- optlang/tests/test_expression_parsing.py
+++ optlang/tests/test_expression_parsing.py
@@ -7,6 +7,20 @@ from optlang.expression_parsing import parse_optimization_expression
 import unittest
 
 
+def _quad_terms_to_expression(terms):
+    args = []
+    for term, coef in terms.items():
+        term = list(term)
+        args.append(coef * term[0] * term[-1])
+    return sum(args)
+
+
+def _compare_term_dicts(test_case, dict1, dict2):
+    for term, coef1 in dict1.items():
+        coef2 = dict2[term]
+        test_case.assertEqual(coef1 - coef2, 0)
+
+
 class ExpressionParsingTestCase(unittest.TestCase):
     def setUp(self):
         self.vars = [Variable(name) for name in ["x", "y", "z", "v", "w"]]
@@ -22,8 +36,8 @@ class ExpressionParsingTestCase(unittest.TestCase):
 
         self.assertEqual(offset_const, 0)
         self.assertEqual(offset_obj, offset)
-        self.assertEqual(linear_terms_const, target)
-        self.assertEqual(linear_terms_obj, target)
+        _compare_term_dicts(self, linear_terms_const, target)
+        _compare_term_dicts(self, linear_terms_obj, target)
         self.assertEqual(quad_terms_const, {})
         self.assertEqual(quad_terms_obj, {})
 
@@ -41,8 +55,10 @@ class ExpressionParsingTestCase(unittest.TestCase):
         self.assertEqual(offset_obj, offset)
         self.assertEqual(linear_terms_const, {})
         self.assertEqual(linear_terms_obj, {})
-        self.assertEqual(quad_terms_const, target)
-        self.assertEqual(quad_terms_obj, target)
+        _compare_term_dicts(self, quad_terms_const, target)
+        _compare_term_dicts(self, quad_terms_obj, target)
+        self.assertEqual((_quad_terms_to_expression(quad_terms_obj) - (expr - offset)).expand(), 0)
+        self.assertEqual((_quad_terms_to_expression(quad_terms_const) - (expr - offset)).expand(), 0)
 
     def test_parse_non_expanded_quadratic_expression(self):
         x, y, z = self.vars[:3]
@@ -57,7 +73,7 @@ class ExpressionParsingTestCase(unittest.TestCase):
 
         self.assertEqual(offset_const, -4)
         self.assertEqual(offset_obj, -4 + offset)
-        self.assertEqual(linear_terms_const, linear_target)
-        self.assertEqual(linear_terms_obj, linear_target)
-        self.assertEqual(quad_terms_const, target)
-        self.assertEqual(quad_terms_obj, target)
+        _compare_term_dicts(self, linear_terms_const, linear_target)
+        _compare_term_dicts(self, linear_terms_obj, linear_target)
+        _compare_term_dicts(self, quad_terms_const, target)
+        _compare_term_dicts(self, quad_terms_obj, target)
