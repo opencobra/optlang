3bd8eb7978456e8d339137953c041ce3dd9d4e54
Moritz E. Beber <midnighter@posteo.net>
2020-09-06 09:39:23 +0200

    Replace Travis and AppVeyor with GitHub Actions (#217)
    * chore: remove unused codeclimate config
    
    * chore: remove AppVeyor config
    
    * chore: replace contributing guide
    
    * chore: add issue templates
    
    * chore: add a pull request template
    
    * chore: add a support guide
    
    * chore: remove old notes
    
    * chore: remove superfluous requriements file
    
    * chore: add a code of conduct
    
    * chore: remove AppVeyor scripts
    
    * chore: create new setup in src
    
    * chore: add GitHub workflow
    
    * docs: update readme badges
    
    * chore: use codecov Python tool for now
    
    * chore: fully remove Travis
    
    * chore: fix codecov
    
    * chore: fix branch name
    
    * chore: exclude Python 3.5 on Mac OS
    
    * chore: test symengine on full matrix
    
    * chore: comment coverage section
    
    At the moment a separate `.coveragerc` still exists.
    
    * fix: change TRAVIS to agnostic CI
    
    * chore: fix netlib skip on CI
    
    * chore: exclude symengine from Python 2.7

--- setup.py
+++ setup.py
@@ -13,66 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 
-from setuptools import setup, find_packages
-from itertools import chain
+"""Set up the optlang package."""
+
 
 import versioneer
-
-versioneer.VCS = 'git'
-versioneer.versionfile_source = 'optlang/_version.py'
-versioneer.versionfile_build = 'optlang/_version.py'
-versioneer.tag_prefix = ''  # tags are like 1.2.0
-versioneer.parentdir_prefix = 'optlang-'  # dirname like 'myproject-1.2.0'
-
-# Run
-# pandoc --from=markdown --to=rst README.md -o README.rst
-# from time to time, to keep README.rst updated
-with open('README.rst', 'r') as f:
-    description = f.read()
-
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if on_rtd:
-    requirements = []
-else:
-    # requirements = ['sympy>=1.0.0', 'six>=1.9.0']
-    with open("requirements.txt") as infile:
-        requirements = infile.read().splitlines()
+from setuptools import setup
 
 
-extra_requirements = {
-    'test': [
-        'nose>=1.3.7',
-        'colorama==0.3.9',
-        'rednose>=0.4.3',
-        'coverage>=4.0.3',
-        'jsonschema>=2.5'
-    ],
-}
-extra_requirements['all'] = list(set(chain(*extra_requirements.values())))
-
-setup(
-    name='optlang',
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    packages=find_packages(),
-    install_requires=requirements,  # from requirements.txt
-    extras_require=extra_requirements,
-    test_suite='nose.collector',
-    author='Nikolaus Sonnenschein',
-    author_email='niko.sonnenschein@gmail.com',
-    description='Formulate optimization problems using sympy expressions and solve them using interfaces to third-party optimization software (e.g. GLPK).',
-    license='Apache License Version 2.0',
-    url='https://github.com/biosustain/optlang',
-    long_description=description,
-    keywords=['optimization', 'sympy', 'mathematical programming', 'heuristic optimization'],
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'License :: OSI Approved :: Apache Software License',
-    ],
-)
+# All other arguments are defined in `setup.cfg`.
+setup(version=versioneer.get_version(), cmdclass=versioneer.get_cmdclass())
